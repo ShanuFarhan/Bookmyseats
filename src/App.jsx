00361ddcef7ee405ee03571012627bc9aa5033e5
ShanuFarhan <shanumet@gmail.com>
2023-10-07 20:31:09 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css'
 import Footer from './Components/Footer/Footer'
 import Header from './Components/Header/Header'
 import Layout from './Components/Layout/Layout'
-import SeatSelector from './Components/NewMethod/SeatSelector'
 
 function App({handleBookSeats}) {
 
